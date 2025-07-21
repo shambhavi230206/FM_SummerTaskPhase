17ea90f67f6805cb3e4975c4565e8ea75d30e438
shambhavi230206 <dasshipra230206@gmail.com>
2025-07-21 17:09:45 +0530

    Update Q8.c

--- Q8.c
+++ Q8.c
@@ -1,26 +1,36 @@
 #include <stdio.h>
 
 int main() {
-    int arr[100], n, i;
-    int *p, temp;
+int n, i;
+    int arr[100];
+int *p, temp;
 
-    scanf("%d", &n);
+    printf("Enter the number of elements (max 100): ");
+scanf("%d", &n);
 
-    for(i = 0; i < n; i++) {
-        scanf("%d", &arr[i]);
+    if (n <= 0 || n > 100) {
+        printf("Invalid number of elements.\n");
+            return 1;
     }
 
-    p = arr;
+  printf("Enter %d elements:\n", n);
+    for(i = 0; i < n; i++) {
+      scanf("%d", &arr[i]);
+    }
+
+p = arr;
 
     for(i = 0; i < n - 1; i = i + 2) {
-        temp = *(p + i);
+     temp = *(p + i);
         *(p + i) = *(p + i + 1);
-        *(p + i + 1) = temp;
+  *(p + i + 1) = temp;
     }
 
-    for(i = 0; i < n; i++) {
-        printf("%d ", *(p + i));
+printf("Array after swapping adjacent elements:\n");
+        for(i = 0; i < n; i++) {
+    printf("%d ", *(p + i));
     }
+printf("\n");
 
     return 0;
 }
