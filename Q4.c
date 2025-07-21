c1a092484dcbeefe1b2bf9a6067a9537576e797c
shambhavi230206 <dasshipra230206@gmail.com>
2025-07-21 17:06:58 +0530

    Update Q4.c

--- Q4.c
+++ Q4.c
@@ -2,20 +2,32 @@
 #include <string.h>
 
 int main() {
-    char password[20];
-    int attempts;
+    char correctPassword[50],inputPassword[50];
+    int attempts = 0;
+    printf("Set your password: ");
+    fgets(correctPassword, sizeof(correctPassword), stdin);
+    size_t len = strlen(correctPassword);
+    if (len > 0 && correctPassword[len - 1] == '\n') {
+        correctPassword[len - 1] = '\0';
+    }
 
-    attempts = 0;
+    while (attempts < 3)
+     {
+        printf("Enter password (Attempt %d of 3): ", attempts + 1);
+        fgets(inputPassword, sizeof(inputPassword), stdin);
 
-    while (attempts < 3) {
-        scanf("%s", password);
+       
+        len = strlen(inputPassword);
+        if (len > 0 && inputPassword[len - 1] == '\n') {
+            inputPassword[len - 1] = '\0';
+        }
 
-        if (strcmp(password, "pass123") == 0) {
+        if (strcmp(inputPassword, correctPassword) == 0) {
             printf("Login successful\n");
             break;
         } else {
             printf("Wrong password\n");
-            attempts = attempts + 1;
+            attempts++;
         }
     }
 
