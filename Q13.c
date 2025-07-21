d8c593a5a8d800153236bcdb22cab381c7720f2f
shambhavi230206 <dasshipra230206@gmail.com>
2025-07-21 17:14:55 +0530

    Update Q13.c

--- Q13.c
+++ Q13.c
@@ -1,53 +1,43 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
 
 int main() {
-FILE *fp;
-char ch;
-int characters, words, lines, inWord;
-characters = 0; words = 0; lines = 0; inWord = 0;
-fp = NULL; ch = ' ';
+    FILE *file;
+    char filename[100], ch;
+    int characters = 0, words = 0, lines = 0;
+    int inWord = 0;
 
-char filename[100];
-printf("Enter filename: ");
-scanf("%s", filename);
+    printf("Enter the filename to read: ");
+    scanf("%s", filename);
 
-fp = fopen(filename, "r");
-if(fp == NULL) {
-    printf("File not found!\n");
-    return 1;
-}
+    file = fopen(filename, "r");
 
-while(1)
-{
-   ch = fgetc(fp);
-   if(ch == EOF)
-   {
-      break;
-   }
-   characters = characters + 1;
+    if (file == NULL) {
+        printf("Error: Cannot open file %s\n", filename);
+        return 1;
+    }
 
-   if(ch == '\n')
-   {
-      lines = lines + 1;
-   }
-   if(ch == ' ' || ch == '\n' || ch == '\t')
-   {
-      inWord = 0;
-   }
-   else
-   {
-      if(inWord == 0)
-      {
-         words = words + 1;
-         inWord = 1;
-      }
-   }
-}
-fclose(fp);
+    while ((ch = fgetc(file)) != EOF) {
+        characters++;
 
-printf("Characters: %d\n", characters);
-printf("Words: %d\n", words);
-printf("Lines: %d\n", lines);
+        if (ch == '\n')
+            lines++;
 
-return 0;
+        if (isspace(ch)) {
+            inWord = 0; // Outside a word
+        } else if (!inWord) {
+            inWord = 1; // Start of a new word
+            words++;
+        }
+    }
+
+    fclose(file);
+
+    printf("\nFile analysis of '%s':\n", filename);
+    printf("Characters: %d\n", characters);
+    printf("Words     : %d\n", words);
+    printf("Lines     : %d\n", lines);
+
+    return 0;
 }
