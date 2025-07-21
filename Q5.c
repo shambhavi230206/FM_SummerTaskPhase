78f1978f23772a582ff5e5a1dd041ca3d820d5a1
shambhavi230206 <dasshipra230206@gmail.com>
2025-07-21 17:07:44 +0530

    Update Q5.c

--- Q5.c
+++ Q5.c
@@ -1,31 +1,35 @@
 #include <stdio.h>
+#include <math.h>
 
 int main() {
-    int arr[100];
-    int n;
-    int i, j, k;
+    int n, i, j, k;
+    const float epsilon = 0.0001;
 
+    printf("Enter number of elements in array: ");
     scanf("%d", &n);
-
+ float arr[n];
+ printf("Enter %d elements (decimals allowed):\n", n);
     for (i = 0; i < n; i++) {
-        scanf("%d", &arr[i]);
+        scanf("%f", &arr[i]);
     }
 
     for (i = 0; i < n; i++) {
-        for (j = i + 1; j < n; j++) {
-            if (arr[i] == arr[j]) {
-                for (k = j; k < n - 1; k++) {
+      for (j = i + 1; j < n; j++) {
+            if (fabs(arr[i] - arr[j]) < epsilon) {
+           for (k = j; k < n - 1; k++) {
                     arr[k] = arr[k + 1];
                 }
-                n = n - 1;
-                j = j - 1;
-            }
-        }
+          n = n - 1;
+          j = j - 1;
+          }
+     }
     }
 
+    printf("Array after removing duplicates:\n");
     for (i = 0; i < n; i++) {
-        printf("%d ", arr[i]);
+        printf("%.2f ", arr[i]);
     }
+    printf("\n");
 
     return 0;
 }
